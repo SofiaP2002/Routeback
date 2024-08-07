33ef37d0a8a4edaf8efb979c64eb27254da66389
SofiaP2002 <118442841+SofiaP2002@users.noreply.github.com>
2024-08-07 18:56:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
 
     origin:["https://deploy-back-six.vercel.app/"],
     methods:["POST","GET","PUT"],
-    credentials: true
+    
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
